d419055a437b3c350efaa737254564a1b6ade15c
Arek Oleksik <arkadiuszoleksik@gmail.com>
2018-04-21 09:21:13 +0200

    Home component style initial

--- github_app/src/routes/home/home.component.js
+++ github_app/src/routes/home/home.component.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import {Container} from './home.style'
 
 class Home extends Component {
     render() {
         return (
             <div>
+                <Container>
                 home
+                </Container>
             </div>
         );
     }
